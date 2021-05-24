6d4d7e157a842b6a6e87e13304358fc46f9b0806
Olena-Kandibula <bakkara@ukr.net>
2021-05-24 12:07:44 +0300

    +дз 16

--- js/hw-03.js
+++ js/hw-03.js
@@ -34,7 +34,7 @@
 //     rating: 4,
 //     price: 2153,
 //     tags: ["premium", "promoted", "top"], 
-   
+
 //    }
 //    ;
 //    console.log(apartment);//{imgUrl: "https://via.placeholder.com/640x480", descr: "Spacious apartment in the city center", rating: 4, price: 2153, tags: Array(3)}
@@ -138,13 +138,13 @@
 //     price: 2153,
 //     tags: ['premium', 'promoted', 'top'],
 //   };
-  
+
 
 //   const aptRating = apartment.rating;
 //   const aptDescr = apartment.descr;
 //   const aptPrice = apartment.price;
 //   const aptTags = apartment.tags;
- 
+
 // 	console.log(aptRating)//4
 
 
@@ -208,7 +208,7 @@
 //       email: 'henry.carter@aptmail.com',
 //     },
 //   };
-  
+
 //   // Change code below this line
 //   const ownerName = apartment.owner.name;
 //   const ownerPhone = apartment.owner.phone;
@@ -278,7 +278,7 @@
 //     price: 2153,
 //     tags: ['premium', 'promoted', 'top'],
 //   };
-  
+
 //   // Change code below this line
 //   const aptRating = apartment['rating'];
 //   const aptDescr = apartment['descr'];
@@ -385,7 +385,7 @@
 //       email: 'henry.carter@aptmail.com'
 //     }
 //   };
-  
+
 //   apartment.area = 60;
 //   apartment.rooms = 3;
 //   apartment.location = {
@@ -437,20 +437,380 @@
 // price, image и tags со значениями из переменных с аналогичными именами.
 //  Обязательно используй синтаксис коротких свойств.
 
-const name = 'Repair Droid';
-const price = 2500;
-const image = 'https://via.placeholder.com/640x480';
-const tags = ['on sale', 'trending', 'best buy'];
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
 
-const product = {
-   
-  price,
-  image, 
-  tags,
-  name
+// const product = {
 
-  };
+//  name, price, image, tags
 
-console.log(product.price);//Repair Droid
+//   };
+
+// console.log(product.name);//Repair Droid
 
 // =====================   9  ===========================
+
+// Вычисляемые свойства
+// Бывают ситуации, когда при объявлении обьекта необходимо добавить
+// свойство с именем, которое мы заранее не знаем, потому что оно
+// хранится как значение переменной или как результат выполнения функции.
+
+// Раньше для этого необходимо было сначала создать объект, а
+// потом добавлять свойства через квадратные скобки, что не
+// совсем удобно.
+
+// const propName = "name";
+// const user = {
+//   age: 25
+// };
+
+// user[propName] = "Генри Сибола";
+// console.log(user.name); // "Генри Сибола"
+// Синтаксис вычисляемых свойств(computed properties) помогает
+// избежать лишнего кода и в некоторых случаях упростить его.
+// Значением вычисляемого свойства может быть любое валидное
+// выражение.
+
+// const propName = "name";
+// const user = {
+//   age: 25,
+//   // Имя этого свойства будет взято из значения переменной propName
+//   [propName]: "Генри Сибола"
+// };
+
+// console.log(user.name); // "Генри Сибола"
+// Задание
+// Дополни код объявления объекта credentials так, чтобы в
+// результате у него были два свойства: email и password,
+//   имена которых хранятся в переменных emailInputName и
+// passwordInputName.
+
+// Значением свойства email должна быть строка
+// "henry.carter@aptmail.com", а значением свойства
+// password - строка "jqueryismyjam".
+
+
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Change code below this line
+
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam"
+
+//   // Change code above this line
+// };
+
+
+// =====================   10  ===========================
+// Цикл for...in
+//   В отличии от массива или строки, объект - это не итерируемая
+// сущность, то есть его нельзя перебрать циклами for
+//  или for...of.Для перебора объектов используется специальный
+// цикл for...in, который перебирает ключи объекта object.
+
+// for (key in object) {
+//   // инструкции
+// }
+// Переменная key доступная только в теле цикла.На каждой
+// итерации в неё будет записано значение ключа(имя) свойства.
+// Для того чтобы получить значение свойства с таким ключом(именем),
+//   используется синтаксис квадратных скобок.
+
+// const book = {
+//   title: 'Последнее королевство',
+//   author: 'Бернард Корнуэлл',
+//   genres: ['историческая проза', 'приключения'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значение свойства с таким ключом
+//   console.log(book[key]);
+// }
+
+
+// Задание
+// Перебери объект apartment используя цикл for...in и
+// запиши в массив keys все его ключи, а в массив values
+// все значения его свойств.
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+
+// // Change code below this line
+// for (const key in apartment) {
+
+//   //console.log(key);
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);//(3) ["descr", "rating", "price"]
+// console.log(values);//(3) ["Spacious apartment in the city centre", 4, 2153]
+
+// =====================   11  ===========================
+
+// Метод hasOwnProperty()
+// Разберём концепцию собственных и несобственных свойств объекта и научимся
+// правильно использовать цикл for...in.
+
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = 'Манго';
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
+// Метод Object.create(animal) создаёт и возвращает новый
+// объект, связывая его с объектом animal.Поэтому можно
+// получить значение свойства legs обратившись к нему как dog.
+//   legs, хотя его нет в объекте dog - это несобственное свойство
+// из объекта animal.
+
+// Оператор in, который используется в цикле for...in, не делает
+// различия между собственными и несобственными свойствами объекта.
+// Эта особенность мешает, так как мы всегда хотим перебрать только
+// собственные свойства.Для того чтобы узнать есть в объекте собственное
+// свойство или нет, используется метод hasOwnProperty(key), который
+// возвращает true или false.
+
+// // ❌ Возвращает true для всех свойств
+// console.log('name' in dog); // true
+// console.log('legs' in dog); // true
+
+// // ✅ Возвращает true только для собственных свойств
+// console.log(dog.hasOwnProperty('name')); // true
+// console.log(dog.hasOwnProperty('legs')); // false
+// Поэтому при переборе циклом for...in необходимо на каждой итерации
+// добавить проверку на собственное свойство.Даже если сейчас мы уверены
+// в том что у объекта нет несобственных свойств, это оградит от возможных
+// ошибок в будущем.
+
+// const book = {
+//   title: 'Последнее королевство',
+//   author: 'Бернард Корнуэлл',
+//   genres: ['историческая проза', 'приключения'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Если это собственное свойство - выполняем тело if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+//   // Если это не собственное свойство - ничего не делаем
+// }
+// Задание
+// Выполни рефакторинг решения предыдущего задания добавив в
+// цикл for...in проверку на собственное свойство.
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city centre';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
+
+
+
+// =====================   12  ===========================
+
+// Задача. Подсчёт свойств
+// Задание
+// Напиши функцию countProps(object), которая считает и возвращает количество
+// собственных свойств объекта в параметре object.Используй переменную
+// propCount для хранения количества свойств объекта.
+
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+
+//   for (const key in object) {
+
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;      
+//     }
+//   }
+//   // Change code above this line
+//   return propCount;
+// }
+
+// countProps({});
+// countProps({ name: "Mango", age: 2 });
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
+
+// =====================   13  ===========================
+// Метод Object.keys()
+// У встроенного класса Object есть несколько полезных методов для
+// работы с объектами.Первый из них это Object.keys(obj), который
+// принимает объект и возвращает массив ключей его собственных свойств.
+// Если в объекте нет свойств, метод вернёт пустой массив.
+
+// const book = {
+//   title: 'Последнее королевство',
+//   author: 'Бернард Корнуэлл',
+//   genres: ['историческая проза', 'приключения'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+// Скомбинировав результат Object.keys() и цикл for...of можно удобно
+// перебрать собственные свойства объекта, не прибегая к использованию
+// архаического цикла for...in с проверками принадлежности свойств.
+
+// const book = {
+//   title: 'Последнее королевство',
+//   author: 'Бернард Корнуэлл',
+//   genres: ['историческая проза', 'приключения'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значение свойства
+//   console.log(book[key]);
+// }
+// Мы перебираем массив ключей объекта и на каждой итерации
+// получаем значение свойства с таким ключом.
+
+// Задание
+// Перебери объект apartment используя метод Object.keys() и 
+//цикл for...of.Запиши в переменную keys массив ключей собственных
+//свойств объекта apartment, и добавь в массив values все значения его свойств.
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+
+//   values.push(apartment[key]);
+// }
+
+//   console.log(values);//Array(3)
+//0: "Spacious apartment in the city centre"
+// 1: 4
+// 2: 2153
+// length: 3
+
+// =====================   14  ===========================
+
+// Задача. Подсчёт свойств 2.0
+// Задание
+// Выполни рефакторинг функции countProps(object) используя метод Object.keys() и,
+//     возможно, цикл for...of.
+
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//     propCount += 1;
+//   }
+//   return propCount;
+//   // Change code above this line
+// }
+// countProps({ name: "Mango", age: 2 });//(2) ["name", "age"]
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });//(3) ["mail", "isOnline", "score"]
+
+
+// =====================   15  ===========================
+// Метод Object.values()
+// Если метод Object.keys(obj) возвращает массив ключей собственных свойств
+// объекта, то метод Object.values(obj) возвращает массив значений его
+// собственных свойств.Если в объекте нет свойств, метод Object.values(obj)
+// вернёт пустой массив.
+
+// const book = {
+//   title: "Последнее королевство",
+//   author: "Бернард Корнуэлл",
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ["title", "author", "genres", "rating"]
+
+// const values = Object.values(book);
+// console.log(values); // ["Последнее королевство", "Бернард Корнуэлл", 8.38]
+// Массив значений свойств также можно перебрать циклом for...of,
+//   например для получения общей суммы числовых значений.
+
+// Задание
+// Запиши в переменную keys массив ключей собственных свойств объекта
+// apartment, а в переменную values массив всех значений его свойств.
+// Используй методы Object.keys() и Object.values().
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city centre',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// console.log(keys);//(3) ["descr", "rating", "price"]
+// console.log(values);//(3) ["Spacious apartment in the city centre", 4, 2153]
+
+// =====================   16  ===========================
+// Задача. Расходы на зарплату
+// Задание
+// Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат,
+//   где имя свойства это имя сотрудника, а значение свойства это зарплата.Функция
+// должна рассчитать общую сумму зарплат сотрудников и вернуть её.Используй
+// переменную totalSalary для хранения общей суммы зарплаты.
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+  
+// for (const value of values) {
+//      totalSalary += value;
+//    }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// countTotalSalary({ mango: 100, poly: 150, alfred: 80 });//330
+
+
+// =====================   17  ===========================
+
+// =====================   18  ===========================
+
+// =====================   19  ===========================
+
+// =====================   20  ===========================
