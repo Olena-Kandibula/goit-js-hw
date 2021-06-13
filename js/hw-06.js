21113c6575ec0ec8b8040ed2dc5130755b0cec55
Olena-Kandibula <bakkara@ukr.net>
2021-06-13 22:24:58 +0300

    hw-06--21

--- js/hw-06.js
+++ js/hw-06.js
@@ -1144,7 +1144,7 @@
 
 // Задача. Пользователи в возрастной категории
 // Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
-
+//  const users=
 // [
 //   {
 //     name: 'Moore Hensley',
@@ -1217,15 +1217,212 @@
 //     age: 39
 //   }
 // ]
-// Задание
-// Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она 
-// возвращала массив пользователей, возраст которых (свойство age) попадает в
-//  промежуток от minAge до maxAge.
+// // Задание
+// // Дополни функцию getUsersWithAge(users, minAge, maxAge) так, чтобы она 
+// // возвращала массив пользователей, возраст которых (свойство age) попадает в
+// //  промежуток от minAge до maxAge.
+
+
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//     const adultUser = users.filter(user => user.age > minAge && user.age < maxAge);
+//    return adultUser;
+//    };
+
+//    Тесты
+// Объявлена переменная getUsersWithAge.
+// Переменной getUsersWithAge присвоена стрелочная функция с параметрами (users, minAge, maxAge).
+// Для перебора параметра users используется метод filter().
+// Если значение параметров minAge и maxAge равны 20 и 30 соотвественно, функция возвращает массив объектов пользователей с именами Ross Vazquez, Elma Head и Carey Barr.
+// Если значение параметров minAge и maxAge равны 30 и 40 соотвественно, функция возвращает массив объектов пользователей с именами Moore Hensley, Sharlene Bush, Blackburn Dotson, Sheree Anthony.
+// Если значение параметров minAge и maxAge равны 80 и 100 соотвественно, функция возвращает пустой массив.
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+
 
 // ============== hw -06--20 ============
+// Задача. Пользователи с другом
+// Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+//     const users =
+// [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//     age: 37
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//     age: 34
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//     age: 24
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//     age: 21
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//     age: 27
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//     age: 38
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//     age: 39
+//   }
+// ]
+// // Задание
+// // Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала массив пользователей у которых есть друг с именем в параметре friendName. Массив друзей пользователя хранится в свойстве friends.
+
+// const getUsersWithFriend = (users, friendName) => {
+//   const frendOfFrends = users.filter( user => user.friends.includes(friendName));
+//    return frendOfFrends;   
+//  };
+
+// Тесты
+// Объявлена переменная getUsersWithFriend.
+// Переменной getUsersWithFriend присвоена стрелочная функция с параметрами (users, friendName).
+// Для перебора параметра users используется метод filter().
+// Если значение параметра friendName это строка 'Briana Decker', функция возвращает массив объектов пользователей с именами Sharlene Bush и Sheree Anthony.
+// Если значение параметра friendName это строка 'Goldie Gentry', функция возвращает массив объектов пользователей с именами Elma Head и Sheree Anthony.
+// Если значение параметра friendName это строка 'Adrian Cross', функция возвращает пустой массив.
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
 
 
 // ============== hw -06--21 ============
+// Задача. Список друзей
+// Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+const  users =
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
+  }
+]
+// Задание
+// Дополни функцию getFriends(users) так, чтобы она возвращала массив друзей 
+// всех пользователей (свойство friends). У нескольких пользователей могут быть 
+// одинаковые друзья, сделай так чтобы возвращаемый массив не содержал повторений.
+
+const getFriends = (users) => { 
+
+const unicUserFriends = users.flatMap(user => user.friends)
+.filter((nameFrend, index) => userFriends.indexOf(nameFrend) === index );
+return userFriends;
+
+};
+
+console.log(getFriends(users,[]));
+
 
 // ============== hw -06--22 ============
 
